2ae3fb8643c4758e36305bdc1ff1ca2b5f3c888a
Viola Hu <lovelyviolaat@hotmail.com>
2019-10-02 13:30:54 +1000

    start mergesort()

--- js/main18.js
+++ js/main18.js
@@ -1,3 +1,8 @@
-console.log('main18 loaded!');
+console.log('main18 loaded! Merge Sort');
 
-// version 1
+// Merge Sort - is a Devide and Conquer algorithm.
+// It devides input array in two halves,
+// calls itself for the two halves and then merges the two sorted halves.
+// Two functions are involved in this algorithm
+// The merge() functio is used for merging two halves.
+// The Mergesort() function recursively calls itself to devide the array till size becomes one.
